6106db9dddcca8369e6578607dc056caf25223e9
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2022-05-25 01:06:00 +0300

    -

--- src/Manage/templates/header.php
+++ src/Manage/templates/header.php
@@ -2,5 +2,5 @@
     use Colibri\App;
 ?>
 <script src="/res/codemirror/lib/codemirror.js" type="text/javascript"></script>  
-<script src="/res/tinymce/<?=(App::$isDev ? 'tinymce.js' : 'tinymce.min.js')?>" type="text/javascript"></script>  
+<script src="/res/tinymce/tinymce.js" type="text/javascript"></script>  
 <link rel="stylesheet" href="/res/codemirror/lib/codemirror.css" type="text/css" />
