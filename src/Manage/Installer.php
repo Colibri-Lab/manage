d76d73b6af1c6e7f0495c1d811c0357a0b01724a
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2023-06-02 15:16:47 +0300

    -

--- src/Manage/Installer.php
+++ src/Manage/Installer.php
@@ -110,7 +110,7 @@ class Installer
         // копируем конфиг
         print_r('Копируем файл конфигурации' . "\n");
         self::_copyOrSymlink($mode, $configPath, $configDir, 'module-' . $mode . '.yaml', 'manage.yaml');
-        self::_copyOrSymlink($mode, $configPath, $configDir, 'module-langtexts.yaml', 'manage-langtexts.yaml');
+        self::_copyOrSymlink($mode, $configPath, $configDir, 'manage-langtexts.yaml', 'manage-langtexts.yaml');
 
         print_r('Встраиваем модуль' . "\n");
         self::_injectIntoModuleConfig($configDir . 'modules.yaml');
