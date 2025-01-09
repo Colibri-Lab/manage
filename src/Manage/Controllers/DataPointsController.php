bc3fa536c8c8cf415728999c20838bffdbd103f7
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2025-01-09 11:04:23 +0400

    -

--- src/Manage/Controllers/DataPointsController.php
+++ src/Manage/Controllers/DataPointsController.php
@@ -24,6 +24,7 @@ use Colibri\Utils\Config\Config;
 use Colibri\Utils\Config\ConfigException;
 use Colibri\Data\DataAccessPoint;
 use App\Modules\Security\Module as SecurityModule;
+use Throwable;
 
 /**
  * Datapoint access controller
@@ -46,17 +47,21 @@ class DataPointsController extends WebController
 
         $result = [];
         foreach (App::$dataAccessPoints->accessPoints->points as $name => $point) {
-            $dtp = App::$dataAccessPoints->Get($name);
-            $point->allowedTypes = $dtp->allowedTypes;
-            $point->hasIndexes = $dtp->hasIndexes;
-            $point->fieldsHasPrefix = $dtp->fieldsHasPrefix;
-            $point->hasVirtual = $dtp->hasVirtual;
-            $point->hasMultiFieldIndexes = $dtp->hasMultiFieldIndexes;
-            $point->indexTypes = $dtp->indexTypes;
-            $point->indexMethods = $dtp->indexMethods;
-            $point->jsonIndexes = $dtp->jsonIndexes;
-            $point->dbms = $dtp->dbms;
-            $result[$name] = $point;
+            try {
+                $dtp = App::$dataAccessPoints->Get($name);
+                $point->allowedTypes = $dtp->allowedTypes;
+                $point->hasIndexes = $dtp->hasIndexes;
+                $point->fieldsHasPrefix = $dtp->fieldsHasPrefix;
+                $point->hasVirtual = $dtp->hasVirtual;
+                $point->hasMultiFieldIndexes = $dtp->hasMultiFieldIndexes;
+                $point->indexTypes = $dtp->indexTypes;
+                $point->indexMethods = $dtp->indexMethods;
+                $point->jsonIndexes = $dtp->jsonIndexes;
+                $point->dbms = $dtp->dbms;
+                $result[$name] = $point;
+            } catch(Throwable $e) {
+                
+            }
         }
 
         return $this->Finish(200, 'ok', $result);
