17cddd95e7bffd2bfc98aa00c99ec32c46444668
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2025-08-18 13:03:12 +0300

    -

--- src/Manage/templates/header.php
+++ src/Manage/templates/header.php
@@ -4,3 +4,7 @@
 <script src="/res/codemirror/lib/codemirror.js" type="text/javascript"></script>  
 <script src="/res/tinymce/tinymce.js" type="text/javascript"></script>  
 <link rel="stylesheet" href="/res/codemirror/lib/codemirror.css" type="text/css" />
+
+<link
+	href="https://fonts.googleapis.com/css2?family=Roboto+Flex:wght@100;200;300;400;500;600;700;800;900;1000&display=swap"
+	rel="stylesheet">
