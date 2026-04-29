ac58ac6435a79dd89080adc612db774a18138060
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2026-04-29 14:49:59 +0400

    -

--- src/Manage/.Bundle/0.UI/05.TinyMCETextArea.js
+++ src/Manage/.Bundle/0.UI/05.TinyMCETextArea.js
@@ -327,6 +327,7 @@ App.Modules.Manage.UI.TinyMCETextArea = class extends Colibri.UI.Forms.TextArea 
 
                 tinymce.init({
                     selector: '#' + this._controlElementId,
+                    api_key: 'x8s6gaeksejxxh7bk4vz48go06saa27m2toqons8hzsm8wjo',
                     // skin: 'nulla',
                     relative_urls: false,
                     remove_script_host: true,
